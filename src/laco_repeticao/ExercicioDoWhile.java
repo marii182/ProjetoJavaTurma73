6e38d10ac612982173a777136a51b5b73c997024
marii182 <maria.sivaxavier18@gmail.com>
2024-02-28 16:06:03 -0300

    Correção do Exercício Do While

--- src/laco_repeticao/ExercicioDoWhile.java
+++ src/laco_repeticao/ExercicioDoWhile.java
@@ -5,27 +5,23 @@ import java.util.Scanner;
 public class ExercicioDoWhile {
 
 	public static void main(String[] args) {
-		int  numero, soma = 0;
-		
+		int numero, soma = 0;
+
 		Scanner leia = new Scanner(System.in);
-		
+
 		System.out.println("Digite um número inteiro");
-		numero = leia.nextInt();
-		
+
 		do {
 			numero = leia.nextInt();
 			if (numero > 0) {
-				soma = soma + numero;
-				
-			}
-		}while (numero != 0);
-			
-			System.out.println("A soma dos números positivos digitados é: " +soma);
-			
-			leia.close();
-		}
+				soma += numero;
 
-			
+			}
+		} while (numero != 0);
+
+		System.out.println("A soma dos números positivos digitados é: " + soma);
+
+		leia.close();
 	}
 
-
+}
